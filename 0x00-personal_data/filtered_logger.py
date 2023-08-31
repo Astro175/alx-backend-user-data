e18f9175429be0ab853fd7ff3d3b92c8f7b29e4c
astro175 <aspark400@gmail.com>
2023-08-31 13:25:40 +0100

    done task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+from typing import List
+"""
+   A function called filter_datum that returns the log
+   message obfuscated
+"""
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+      A function that replaces personal info with
+      redaction
+    """ 
+
+    for field in fields:
+        pattern = f'{field}=.*?{separator}'
+        repl = f'{field}={redaction}{separator}'
+        input_str = message
+        message = re.sub(pattern, repl, input_str)
+    return message
