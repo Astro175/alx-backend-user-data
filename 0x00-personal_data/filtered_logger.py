e348fd965a26ab923a59185c3898b0d74315f417
astro175 <aspark400@gmail.com>
2023-08-31 18:09:02 +0100

    done task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,14 +7,34 @@ from typing import List
    message obfuscated
 """
 
+import logging
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-      A function that replaces personal info with
-      redaction
+    A function that replaces personal info with
+    redaction
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields=None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.field = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.field, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
