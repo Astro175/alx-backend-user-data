d0156426764f8e641ee4062bc0acd76358d62349
astro175 <aspark400@gmail.com>
2023-09-02 23:10:09 +0100

    fixed some errors in the connection variables

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ class RedactingFormatter(logging.Formatter):
     """
       Redacting Formatter class
     """
-    
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
