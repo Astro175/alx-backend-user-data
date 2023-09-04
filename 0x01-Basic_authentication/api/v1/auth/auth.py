997133f76e7fb1214f469457134038d6dfb20ccd
astro175 <aspark400@gmail.com>
2023-09-04 15:46:27 +0100

    added method that determines which path has restricted access

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,23 +3,40 @@
 """
   Module for authentication
 """
+from typing import List, TypeVar
 
 from flask import request
+
 
 class Auth():
     """
       Authentication class
     """
-    
+
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         """
           Method that depends which path requires
           authentication or not
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
 
-    def def authorization_header(self, request=None) -> str:
+        len_path = len(path)
+
+        if path[len_path - 1] == '/':
+            slashed_path = path
+        else:
+            slashed_path = path + '/'
+
+        if slashed_path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
         """"
            Method for authorization_header
         """
