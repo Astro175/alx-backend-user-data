34261cdfdc561a3a943fd5c32d09de920dfcbf77
astro175 <aspark400@gmail.com>
2023-09-05 12:08:15 +0100

    started building basic_auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+  Module that instantiates an AUth
+  class Model
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+      BasicAuth class
+    """
+    pass
