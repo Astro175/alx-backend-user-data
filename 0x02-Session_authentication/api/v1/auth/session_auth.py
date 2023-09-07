38375732cce21b399c5e0e16a1b0876a3ff22dfe
astro175 <aspark400@gmail.com>
2023-09-07 21:07:53 +0100

    added solution to task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+  Module for session authentication
+"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+class SessionAuth(Auth):
+    """
+      Class for session auth
+    """
+    pass
