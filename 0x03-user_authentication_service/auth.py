e78acaedce1d699c2e6c5ada97563c9af36706e8
astro175 <aspark400@gmail.com>
2023-09-14 23:57:53 +0100

    done task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,31 @@
 
 """Module that hashes a password with bcrypt"""
 import bcrypt
+from db import DB
+from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Should take mandatory email and password string
+        arguments and return a User object."""
+
+        try:
+            old_user = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            hashed_pw = _hash_password(password)
+            user = self._db.add_user(email, hashed_pw)
+        else:
+            raise ValueError("User {} already exists".format(email))
+        return user
 
 
 def _hash_password(password: str) -> bytes:
