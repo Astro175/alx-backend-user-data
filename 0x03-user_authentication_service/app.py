fd2efcd01c06cd61d9fe464c41526e6637b90296
astro175 <aspark400@gmail.com>
2023-09-15 19:21:26 +0100

    added solution to task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basiv flask app"""
 
 from auth import Auth
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -44,6 +44,20 @@ def login():
     response = jsonify(payload)
     response.set_cookie('session_id', id)
     return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """Find the user with the requested session ID. If the user
+    exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status."""
+
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        return abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
