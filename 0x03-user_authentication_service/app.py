b88b47ace05ba379c0157ba62df8a46d43a72f4c
astro175 <aspark400@gmail.com>
2023-09-15 16:58:23 +0100

    added solution to task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basiv flask app"""
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -27,6 +27,23 @@ def users():
     except ValueError:
         payload = {"message": "email already registered"}
         return jsonify(payload), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Checks for credentials and logs in user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    status = AUTH.valid_login(email, password)
+
+    if not status:
+        abort(401)
+    id = AUTH.create_session(email)
+    payload = {"email": "<user email>", "message": "logged in"}
+    response = jsonify(payload)
+    response.set_cookie('session_id', id)
+    return response
 
 
 if __name__ == "__main__":
